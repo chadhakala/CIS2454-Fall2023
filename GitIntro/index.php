c92e2e28691aaea277af297400e53640eaca40c2
procyonfrwk <hakala.chad@student.oaklandcc.edu>
2023-09-03 01:58:13 -0400

    gitintro

---
+++ GitIntro/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+          <?php
+       echo "<h2>Hello World!</h2>";
+       echo "Chad Hakala, Software Engineering, I'm taking this class because I'm wanting to be able to do web dev stuff 
+       and I'm really excited to finally be learning PHP.";
+       echo "<br> i've been programming for about a year";
+       echo "<br> I like tinkering with IOT projects and skateboarding.";
+       echo "<br> This fall I don't really have any plans. Would like to launch a media server with a paid version of unraid.";
+       echo "<br> Something you should know about me is that I read gosh heckin' manual"
+               ?>
+    </body>
+</html>
