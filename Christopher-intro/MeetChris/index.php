a942ec7680c88af678dfac5f47198f0995958eba
chadhakala <hakala.chad@student.oaklandcc.edu>
2023-09-19 20:57:29 -0400

    relocated

---
+++ Christopher-intro/MeetChris/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Christopher is curtrently finishing his associates in Software Engineeringing and plans to further his education
+        beyond OCC to OU most likely because seems like best for transfering.";
+        echo "<br> Chris has been learning programming since he was a junior in a trade program in high school and has moved onto college to aquire more skills;
+        <br>so, in total, Chris currently has approximately two or three years experience.";
+        echo "<br> Chris's hobbies include gaming. He does a lot of gaming with his brothers and sisters over the internet.
+        <br>He also gave high regards to Baldurs Gate 3 which I now wish to own.";
+        echo "<br> Chris says he doesn't really have any plans for this fall";
+        echo "<br> lastly, Christopher wanted to share that he has plans to see his girlfriend soon for Military graduation soon.";
+        ?>
+    </body>
+</html>
